4d738053eae15d9703641419a0e45c25767b2674
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-09-28 15:01:58 -0500

    initialized student constructor

--- Student.java
+++ Student.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeLevel;
     private int studentID;
     private String courses;
     private int tuitionBalance;
     private int costOfCourse = 600;
 
     //Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter student first name: ");
+        this.firstName = in.nextLine();
 
+        System.out.print("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("Grade Levels:\n1-Freshmen\n2-Sophmore\n3-Junior\n4-Senior\nEnter student grade level: ");
+        this.gradeLevel = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeLevel);
+    }
     //Generate an ID
 
     //Enroll in courses
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,6 +1,8 @@
 public class StudentDatabaseApp {
     
     public static void main(String[] args) {
+        Student student1 = new Student();
+
         //Ask how many new users we want to add
 
         //Create n number of new students
